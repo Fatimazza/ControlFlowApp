a07b8b2194c0879f6df07e29cbf2d260d37bc410
Fatima Az <azzahro.fatima@gmail.com>
2019-11-20 06:26:04 +0700

    [Izza] Add ToDo

--- app/src/main/java/id/co/iconpln/controlflowapp/MainActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/MainActivity.kt
@@ -2,7 +2,6 @@ package id.co.iconpln.controlflowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,6 +16,8 @@ class MainActivity : AppCompatActivity() {
             val angka = etNilai.text.toString().toInt()
             val hitungPangkat = angka * angka
             Toast.makeText(this, hitungPangkat.toString(), Toast.LENGTH_LONG).show()
+
+            //TODO: 2 Delete Toast, Show Operation in Text
         }
     }
 }
