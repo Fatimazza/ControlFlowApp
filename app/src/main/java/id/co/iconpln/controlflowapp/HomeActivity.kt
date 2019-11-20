e21f0749d137060dd41de7c336612c1e597696fc
Fatima Az <azzahro.fatima@gmail.com>
2019-11-20 07:41:00 +0700

    [Izza] Move function inside On Click

--- app/src/main/java/id/co/iconpln/controlflowapp/HomeActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/HomeActivity.kt
@@ -5,31 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        btnCalculation.setOnClickListener {
-            val calculationIntent = Intent(this, MainActivity::class.java)
-            startActivity(calculationIntent)
-        }
-
-        btnClassification.setOnClickListener {
-            Toast.makeText(this, "Classification", Toast.LENGTH_SHORT).show()
-        }
     }
 
     override fun onClick(view: View) {
         when (view.id) {
             R.id.btnCalculation -> {
-
+                val calculationIntent = Intent(this, MainActivity::class.java)
+                startActivity(calculationIntent)
             }
             R.id.btnClassification -> {
-
+                Toast.makeText(this, "Classification", Toast.LENGTH_SHORT).show()
             }
         }
     }
