ddd7d131feac2b25dd77e3918730ff6571742352
Fatima Az <azzahro.fatima@gmail.com>
2019-11-27 07:22:19 +0700

    [Izza] Operation - Add Sealed Class for Operation

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/Operation.kt
@@ -0,0 +1,8 @@
+package id.co.iconpln.controlflowapp
+
+sealed class Operation {
+    class Add(val value: Int) : Operation()
+    class Divide(val value: Int) : Operation()
+    class Multiply(val value: Int) : Operation()
+    class Substract(val value: Int) : Operation()
+}
