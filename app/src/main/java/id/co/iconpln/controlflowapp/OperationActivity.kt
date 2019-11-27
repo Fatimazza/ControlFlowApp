7ad629f61b0e485f08886d0076dddfa88eb11fca
Fatima Az <azzahro.fatima@gmail.com>
2019-11-27 07:51:34 +0700

    [Izza] Operation - Execute Operation for Add, Substratc, Multiply, and Divide

--- app/src/main/java/id/co/iconpln/controlflowapp/OperationActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/OperationActivity.kt
@@ -15,7 +15,6 @@ class OperationActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_operation)
 
         setButtonClickListener()
-        getInputNumbers()
     }
 
     private fun setButtonClickListener() {
@@ -36,16 +35,28 @@ class OperationActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View) {
         when (view.id) {
             R.id.btnOpAdd -> {
-
+                getInputNumbers()
+                val add = Operation.Add(inputY)
+                val addResult = execute(inputX, add)
+                tvOperationResult.text = addResult.toString()
             }
             R.id.btnOpSubstract -> {
-
+                getInputNumbers()
+                val substract = Operation.Substract(inputY)
+                val substractResult = execute(inputX, substract)
+                tvOperationResult.text = substractResult.toString()
             }
             R.id.btnOpMultiply -> {
-
+                getInputNumbers()
+                val multiply = Operation.Multiply(inputY)
+                val multiplyResult = execute(inputX, multiply)
+                tvOperationResult.text = multiplyResult.toString()
             }
             R.id.btnOpDivide -> {
-
+                getInputNumbers()
+                val divide = Operation.Divide(inputY)
+                val divideResult = execute(inputX, divide)
+                tvOperationResult.text = divideResult.toString()
             }
             R.id.btnOpOperation -> {
 
