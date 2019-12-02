340935a8158d28028d265863718210a3d6fed7ed
Fatima Az <azzahro.fatima@gmail.com>
2019-12-02 12:45:18 +0700

    [Izza] Demo - Open Style Activity

--- app/src/main/java/id/co/iconpln/controlflowapp/DemoActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/DemoActivity.kt
@@ -1,14 +1,21 @@
 package id.co.iconpln.controlflowapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_demo.*
 
 class DemoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
+
+        btnSubmit.setOnClickListener {
+            val styleIntent = Intent(this, StyleActivity::class.java)
+            startActivity(styleIntent)
+        }
     }
 
     override fun onStart() {
