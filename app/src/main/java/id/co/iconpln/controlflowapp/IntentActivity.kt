efe086076f0a891546839c921032282a41328bc6
Fatima Az <azzahro.fatima@gmail.com>
2019-12-04 09:40:11 +0700

    [Izza] Intent - Add Click Listener

--- app/src/main/java/id/co/iconpln/controlflowapp/IntentActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/IntentActivity.kt
@@ -2,11 +2,24 @@ package id.co.iconpln.controlflowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_intent.*
 
-class IntentActivity : AppCompatActivity() {
+class IntentActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent)
+
+        btnMoveActivity.setOnClickListener(this)
+        btnMoveActivityData.setOnClickListener(this)
+        btnMoveActivityObject.setOnClickListener(this)
+        btnMoveForResult.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        
     }
 }
