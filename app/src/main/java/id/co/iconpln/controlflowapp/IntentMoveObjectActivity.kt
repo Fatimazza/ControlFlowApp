f3eb11b53f7233afa9dd1c2b8e9c8e8adfb86488
Fatima Az <azzahro.fatima@gmail.com>
2019-12-04 10:00:17 +0700

    [Izza] Intent Move Object - Prepare Receive Intent

--- app/src/main/java/id/co/iconpln/controlflowapp/IntentMoveObjectActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/IntentMoveObjectActivity.kt
@@ -2,11 +2,32 @@ package id.co.iconpln.controlflowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.co.iconpln.controlflowapp.model.Person
+import kotlinx.android.synthetic.main.activity_intent_move_object.*
 
 class IntentMoveObjectActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_PERSON = "extra_person"
+    }
+
+    private var person = Person()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_move_object)
+
+        getIntentExtras()
+        showData()
+    }
+
+    private fun getIntentExtras() {
+        // person = intent.getParcelableExtra<Person>(EXTRA_PERSON)
+    }
+
+    private fun showData() {
+        val text =
+            "Name ${person.name}, \nAge ${person.age}, \nEmail ${person.email} \nCity ${person.city}"
+        tvObjectReceived.text = text
     }
 }
