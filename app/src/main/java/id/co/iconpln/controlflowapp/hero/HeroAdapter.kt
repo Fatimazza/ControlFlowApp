3b9ba4c20d71b771d880c85cffcdd8950bb27279
Fatima Az <azzahro.fatima@gmail.com>
2019-12-10 09:19:42 +0700

    [Izza] Hero Adapter - Make Adapter Extends View Holder

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/HeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/HeroAdapter.kt
@@ -3,10 +3,10 @@ package id.co.iconpln.controlflowapp.hero
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class HeroAdapter {
+class HeroAdapter: RecyclerView.Adapter<HeroAdapter.HeroViewHolder>() {
 
     inner class HeroViewHolder(private val view:View): RecyclerView.ViewHolder(view) {
 
     }
-    
+
 }
