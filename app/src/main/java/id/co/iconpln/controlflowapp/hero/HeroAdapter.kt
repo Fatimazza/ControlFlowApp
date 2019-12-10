a26d736abdfcb7922c74f3d9b7135ae22fc0d054
Fatima Az <azzahro.fatima@gmail.com>
2019-12-10 09:20:14 +0700

    [Izza] Hero Adapter - Implements Needed Function

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/HeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/HeroAdapter.kt
@@ -1,9 +1,22 @@
 package id.co.iconpln.controlflowapp.hero
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class HeroAdapter: RecyclerView.Adapter<HeroAdapter.HeroViewHolder>() {
+    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: HeroViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     inner class HeroViewHolder(private val view:View): RecyclerView.ViewHolder(view) {
 
