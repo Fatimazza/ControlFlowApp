026bf5066d3547116947982a507d9cdae59c31a0
Fatima Az <azzahro.fatima@gmail.com>
2019-12-10 13:46:36 +0700

    [izza] Hero Adapter - Implements Function

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/HeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/HeroAdapter.kt
@@ -1,25 +1,33 @@
 package id.co.iconpln.controlflowapp.hero
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.model.Hero
 
-class HeroAdapter: RecyclerView.Adapter<HeroAdapter.HeroViewHolder>() {
+class HeroAdapter(val listHero: ArrayList<Hero>) : RecyclerView.Adapter<HeroAdapter.HeroViewHolder>() {
     
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list_hero, parent, false)
+        return HeroViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return listHero.size
     }
 
     override fun onBindViewHolder(holder: HeroViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        
     }
 
     inner class HeroViewHolder(private val view:View): RecyclerView.ViewHolder(view) {
 
+        fun bind(hero: Hero, position: Int) {
+
+        }
     }
 
 }
