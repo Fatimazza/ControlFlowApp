53f19e70ccee4e7784624d30b43622c06a43f9a7
Fatima Az <azzahro.fatima@gmail.com>
2019-12-11 07:33:46 +0700

    [Izza] Hero Grid Adapter - Override needed Function

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/GridHeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/GridHeroAdapter.kt
@@ -1,9 +1,22 @@
 package id.co.iconpln.controlflowapp.hero
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class GridHeroAdapter: RecyclerView.Adapter<GridHeroAdapter.GridHeroViewHolder>() {
+    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridHeroViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: GridHeroViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     inner class GridHeroViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
 
