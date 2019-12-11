ce901f8a29b6a84d8832b49cc00f1c048ef02a22
Fatima Az <azzahro.fatima@gmail.com>
2019-12-11 07:31:50 +0700

    [Izza] Hero Grid Adapter - Make View Holder extends Recycler View Holder

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/GridHeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/GridHeroAdapter.kt
@@ -1,8 +1,11 @@
 package id.co.iconpln.controlflowapp.hero
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
 class GridHeroAdapter {
 
-    inner class GridHeroViewHolder {
+    inner class GridHeroViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
 
     }
 }
