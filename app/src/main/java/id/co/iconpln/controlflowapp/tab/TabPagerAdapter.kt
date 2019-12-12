19ccc19b38d74d4a05cd087f58b7be9b5d2e1167
Fatima Az <azzahro.fatima@gmail.com>
2019-12-12 22:13:43 +0700

    [Izza] Tab Adapter - Override Needed Function

--- app/src/main/java/id/co/iconpln/controlflowapp/tab/TabPagerAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/tab/TabPagerAdapter.kt
@@ -1,10 +1,19 @@
 package id.co.iconpln.controlflowapp.tab
 
 import android.content.Context
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
 class TabPagerAdapter(private val context: Context, fm: FragmentManager)
     : FragmentPagerAdapter(fm) {
     
+    override fun getItem(position: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
