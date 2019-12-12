cf7329c1daf4384f047653694382f2a2dfbb2d27
Fatima Az <azzahro.fatima@gmail.com>
2019-12-12 09:35:12 +0700

    [Izza] Other Fragment - Move to Last Fragment

--- app/src/main/java/id/co/iconpln/controlflowapp/fragments/OtherFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragments/OtherFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 
 import id.co.iconpln.controlflowapp.R
 import kotlinx.android.synthetic.main.fragment_other.*
@@ -32,7 +31,13 @@ class OtherFragment : Fragment(), View.OnClickListener {
     override fun onClick(view: View) {
         when (view.id) {
             R.id.btnLastFragment -> {
-                Toast.makeText(requireContext(), "Last Fragment", Toast.LENGTH_SHORT).show()
+                val fragmentManager = fragmentManager
+                val fragmentTransaction = fragmentManager?.beginTransaction()
+
+                val fragment = LastFragment()
+                fragmentTransaction?.replace(R.id.flContainer, fragment)
+                fragmentTransaction?.addToBackStack(null)
+                fragmentTransaction?.commit()
             }
         }
     }
