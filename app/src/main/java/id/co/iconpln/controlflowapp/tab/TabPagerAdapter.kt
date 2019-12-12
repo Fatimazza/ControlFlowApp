6e3b0e138c9c6075c8902bff3250e2e80d954f48
Fatima Az <azzahro.fatima@gmail.com>
2019-12-12 22:58:13 +0700

    [Izza] Tab Adapter - Change Fragment

--- app/src/main/java/id/co/iconpln/controlflowapp/tab/TabPagerAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/tab/TabPagerAdapter.kt
@@ -7,8 +7,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import id.co.iconpln.controlflowapp.R
-import id.co.iconpln.controlflowapp.fragments.HomeFragment
-import id.co.iconpln.controlflowapp.fragments.OtherFragment
 
 class TabPagerAdapter(private val context: Context, fm: FragmentManager)
     : FragmentPagerAdapter(fm) {
@@ -19,8 +17,8 @@ class TabPagerAdapter(private val context: Context, fm: FragmentManager)
     override fun getItem(position: Int): Fragment {
         var fragment: Fragment? = null
         when (position) {
-            0 -> fragment = HomeFragment()
-            1 -> fragment = OtherFragment()
+            0 -> fragment = FirstFragment()
+            1 -> fragment = SecondFragment()
         }
         return fragment as Fragment
     }
