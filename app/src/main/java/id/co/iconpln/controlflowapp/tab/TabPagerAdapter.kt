3545b623c83729815e5b2499ccc63e2d142187dd
Fatima Az <azzahro.fatima@gmail.com>
2019-12-12 22:12:45 +0700

    [Izza] Tab Adapter - Extends Fragment Pager Adapter

--- app/src/main/java/id/co/iconpln/controlflowapp/tab/TabPagerAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/tab/TabPagerAdapter.kt
@@ -1,5 +1,10 @@
 package id.co.iconpln.controlflowapp.tab
 
-class TabPagerAdapter {
+import android.content.Context
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 
+class TabPagerAdapter(private val context: Context, fm: FragmentManager)
+    : FragmentPagerAdapter(fm) {
+    
 }
