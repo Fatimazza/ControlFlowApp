1c0645232d2042ed05cc9ed88e0c323ddb9a582e
Fatima Az <azzahro.fatima@gmail.com>
2019-12-13 09:47:20 +0700

    [Izza] Last Fragment - Show Toast when Option chosen

--- app/src/main/java/id/co/iconpln/controlflowapp/fragments/LastFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragments/LastFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import id.co.iconpln.controlflowapp.R
 import id.co.iconpln.controlflowapp.StyleActivity
@@ -62,6 +63,10 @@ class LastFragment : Fragment(), View.OnClickListener {
     }
 
     var optionDialogListener: OptionDialogFragment.OnOptionsDialogListener =
-        object: OptionDialogFragment.OnOptionsDialogListener{}
+        object: OptionDialogFragment.OnOptionsDialogListener{
+            override fun onOptionChosen(text: String) {
+                Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+            }
+        }
 
 }
