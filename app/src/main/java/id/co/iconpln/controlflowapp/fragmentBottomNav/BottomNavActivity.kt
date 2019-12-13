77fd9e7130021b4ed48d680b9f9e95370eb87614
Fatima Az <azzahro.fatima@gmail.com>
2019-12-13 08:51:06 +0700

    [Izza] Bottom Nav - Initial Setup Bottom Navigation

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentBottomNav/BottomNavActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentBottomNav/BottomNavActivity.kt
@@ -2,12 +2,24 @@ package id.co.iconpln.controlflowapp.fragmentBottomNav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_bottom_nav.*
 
 class BottomNavActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_nav)
+
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+    private val onNavigationItemSelectedListener = object : BottomNavigationView.OnNavigationItemSelectedListener {
+
     }
 }
