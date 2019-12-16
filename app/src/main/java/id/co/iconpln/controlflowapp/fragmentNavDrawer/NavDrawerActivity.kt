730f10ad3c7b302526302d119cdc6c41fa493513
Fatima Az <azzahro.fatima@gmail.com>
2019-12-16 07:29:37 +0700

    [Izza] Nav Drawer - Add Navigation Item Selected Listener

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
@@ -1,19 +1,25 @@
 package id.co.iconpln.controlflowapp.fragmentNavDrawer
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import id.co.iconpln.controlflowapp.R
 import kotlinx.android.synthetic.main.activity_nav_drawer.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
 
-class NavDrawerActivity : AppCompatActivity() {
+class NavDrawerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_drawer)
 
         setupActionBar()
+        navViewDrawer.setNavigationItemSelectedListener(this)
     }
 
     private fun setupActionBar() {
@@ -24,5 +30,21 @@ class NavDrawerActivity : AppCompatActivity() {
         )
         dlDrawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_home -> {
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_profile -> {
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_gallery -> {
+                Toast.makeText(this, "Gallery", Toast.LENGTH_SHORT).show()
+            }
+        }
+        dlDrawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
