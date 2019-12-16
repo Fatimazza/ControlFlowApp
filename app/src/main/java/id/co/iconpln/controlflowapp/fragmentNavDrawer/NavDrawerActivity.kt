462c6326299af3f2836fb0f5cc593b90431cc798
Fatima Az <azzahro.fatima@gmail.com>
2019-12-16 07:11:09 +0700

    [Izza] Nav Drawer - Setup ActionBar for Drawer

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
@@ -1,13 +1,28 @@
 package id.co.iconpln.controlflowapp.fragmentNavDrawer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_nav_drawer.*
+import kotlinx.android.synthetic.main.layout_toolbar.*
 
 class NavDrawerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_drawer)
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this, dlDrawerLayout, toolbar, R.string.app_name, 0
+        )
+        dlDrawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 }
