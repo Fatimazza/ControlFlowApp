65e2e958758e648d1f209da70de0e7ae9aaeda04
Fatima Az <azzahro.fatima@gmail.com>
2019-12-18 09:30:32 +0700

    [Izza] Bottom Sheet - Setup Actionbar

--- app/src/main/java/id/co/iconpln/controlflowapp/bottomSheetDialog/BottomSheetActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/bottomSheetDialog/BottomSheetActivity.kt
@@ -3,11 +3,18 @@ package id.co.iconpln.controlflowapp.bottomSheetDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_bottom_sheet.*
 
 class BottomSheetActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_sheet)
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar)
     }
 }
