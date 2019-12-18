894e16257f2783527a9d78d90f7946ad2474830f
Fatima Az <azzahro.fatima@gmail.com>
2019-12-18 07:16:25 +0700

    [Izza] Nav Drawer - Fix Import

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
@@ -12,6 +12,7 @@ import com.google.android.material.navigation.NavigationView
 import id.co.iconpln.controlflowapp.R
 import id.co.iconpln.controlflowapp.StyleActivity
 import id.co.iconpln.controlflowapp.fragmentTab.FirstFragment
+import id.co.iconpln.controlflowapp.fragmentTab.SecondFragment
 import id.co.iconpln.controlflowapp.hero.ListHeroFragment
 import kotlinx.android.synthetic.main.activity_nav_drawer.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
