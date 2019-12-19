8d95c85e695aa0f1511622957659f9967cfa577e
Fatima Az <azzahro.fatima@gmail.com>
2019-12-19 09:35:46 +0700

    [Izza] Localization - Display Localized Texts

--- app/src/main/java/id/co/iconpln/controlflowapp/LocalizationActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/LocalizationActivity.kt
@@ -3,12 +3,27 @@ package id.co.iconpln.controlflowapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import kotlinx.android.synthetic.main.activity_localization.*
 
 class LocalizationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_localization)
+
+        displayLocalizedTexts()
+    }
+
+    private fun displayLocalizedTexts() {
+        val pokeCount = 3
+        val hello =
+            resources.getString(R.string.hello_world, "Iconplus Squads", pokeCount, "Izza")
+        tvLocaleHallo.text = hello
+
+        val songCount = 5
+        val pluralText =
+            resources.getQuantityString(R.plurals.numberOfSongsAvailable, songCount, songCount)
+        tvLocalePlural.text = pluralText
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
