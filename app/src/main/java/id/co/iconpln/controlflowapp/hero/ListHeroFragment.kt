5bd7457ecbb2682ce59a1e60842598d12fd0a3d7
Fatima Az <azzahro.fatima@gmail.com>
2019-12-19 08:23:39 +0700

    [Izza] List Hero - Get List Hero Data from Resource

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroFragment.kt
@@ -40,7 +40,24 @@ class ListHeroFragment : Fragment() {
 
     private fun setupListHero() {
         rvFragmentListHero.setHasFixedSize(true)
-        listHero.addAll(HeroesData.listDataHero)
+        listHero.addAll(getDataHero())
+    }
+
+    private fun getDataHero(): ArrayList<Hero> {
+        val heroName = resources.getStringArray(R.array.hero_name)
+        val heroDesc = resources.getStringArray(R.array.hero_description)
+        val heroPhoto = resources.getStringArray(R.array.hero_photo)
+
+        val listHero = ArrayList<Hero>()
+        for (position in heroName.indices) {
+            val hero = Hero(
+                heroName[position],
+                heroDesc[position],
+                heroPhoto[position]
+            )
+            listHero.add(hero)
+        }
+        return listHero
     }
 
     private fun showRecyclerViewList() {
