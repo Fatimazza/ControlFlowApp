d42c0c8803e958ee0c610bbe906c3286b262afb4
Fatima Az <azzahro.fatima@gmail.com>
2019-12-20 08:01:40 +0700

    [Izza] Scroll View Pager - Setup Slide View Pager in Activity

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentViewPager/ScrollActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentViewPager/ScrollActivity.kt
@@ -1,13 +1,21 @@
 package id.co.iconpln.controlflowapp.fragmentViewPager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_scroll.*
 
-class ScrollActivity : AppCompatActivity() {
+class ScrollActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll)
+
+        setupSlideViewPager()
+    }
+
+    private fun setupSlideViewPager() {
+        val pagerAdapter = ScrollPagerAdapter(supportFragmentManager)
+        vpScroll.adapter = pagerAdapter
     }
 }
