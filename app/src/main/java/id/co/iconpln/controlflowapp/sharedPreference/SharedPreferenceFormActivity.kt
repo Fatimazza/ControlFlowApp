48413c51513334fe81e72bcfcee58c43c43b4412
Fatima Az <azzahro.fatima@gmail.com>
2020-01-06 08:51:09 +0700

    [Izza] Shared Preference Form - Add Save Button Click Listener

--- app/src/main/java/id/co/iconpln/controlflowapp/sharedPreference/SharedPreferenceFormActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/sharedPreference/SharedPreferenceFormActivity.kt
@@ -2,12 +2,23 @@ package id.co.iconpln.controlflowapp.sharedPreference
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_shared_preference_form.*
 
-class SharedPreferenceFormActivity : AppCompatActivity() {
+class SharedPreferenceFormActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_preference_form)
+
+        btnPrefFormSave.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btnPrefFormSave) {
+            Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show()
+        }
     }
 }
