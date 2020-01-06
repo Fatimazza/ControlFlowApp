4f32ca095ab338ff05d42e2ece7db4cd89c72956
Fatima Az <azzahro.fatima@gmail.com>
2020-01-06 08:41:46 +0700

    [Izza] Shared Preference - Open Form from Button SP Save

--- app/src/main/java/id/co/iconpln/controlflowapp/sharedPreference/SharedPreferenceActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/sharedPreference/SharedPreferenceActivity.kt
@@ -1,13 +1,25 @@
 package id.co.iconpln.controlflowapp.sharedPreference
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_shared_preference.*
 
-class SharedPreferenceActivity : AppCompatActivity() {
+class SharedPreferenceActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_preference)
+
+        btnSpSave.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btnSpSave) {
+            val sharedPrefFormintent = Intent(this, SharedPreferenceFormActivity::class.java)
+            startActivity(sharedPrefFormintent)
+        }
     }
 }
