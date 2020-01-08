0fb4f0432c8461485d79989dfd2e0204763be75f
Fatima Az <azzahro.fatima@gmail.com>
2020-01-08 09:12:32 +0700

    [Izza] Contact Adapter - Add Initial Adapter

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactAdapter.kt
@@ -0,0 +1,12 @@
+package id.co.iconpln.controlflowapp.contact
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter {
+
+    inner class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
