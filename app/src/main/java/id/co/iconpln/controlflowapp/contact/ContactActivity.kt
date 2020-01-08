aaf6ad7707a10cb32abbff9028358988ddbd9961
Fatima Az <azzahro.fatima@gmail.com>
2020-01-08 09:22:00 +0700

    [Izza] Contact View Model - Add and Init

--- app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactActivity.kt
@@ -2,6 +2,7 @@ package id.co.iconpln.controlflowapp.contact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
 import kotlinx.android.synthetic.main.activity_contact.*
@@ -10,11 +11,19 @@ class ContactActivity : AppCompatActivity() {
 
     private lateinit var adapter: ContactAdapter
 
+    private lateinit var contactViewModel: ContactViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
 
+        initViewModel()
         showListContact()
+    }
+
+    private fun initViewModel() {
+        contactViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
+            .get(ContactViewModel::class.java)
     }
 
     private fun showListContact() {
