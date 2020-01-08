34684aba5bf677c178fb3dfaaa9786af75ae3641
Fatima Az <azzahro.fatima@gmail.com>
2020-01-08 09:34:32 +0700

    [Izza] Contact View Model - Initial Request Contact API

--- app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactViewModel.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactViewModel.kt
@@ -3,13 +3,20 @@ package id.co.iconpln.controlflowapp.contact
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
 
 class ContactViewModel : ViewModel() {
 
     private val listContacts = MutableLiveData<ArrayList<Contact>>()
 
     internal fun setContact() {
-        
+        val client = AsyncHttpClient()
+        val listContacts = ArrayList<Contact>()
+        val url = "https://api.androidhive.info/contacts/"
+
+        // Request Contact API
+        client.get(url, object : AsyncHttpResponseHandler(){})
     }
 
     internal fun getContact(): LiveData<ArrayList<Contact>> {
