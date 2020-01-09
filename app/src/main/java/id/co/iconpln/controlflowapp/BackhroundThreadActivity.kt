015709c2ec27a6a34b00c557cbfb5627291e2458
Fatima Az <azzahro.fatima@gmail.com>
2020-01-09 08:39:21 +0700

    [Izza] Background Threads - Add Click Listener

--- app/src/main/java/id/co/iconpln/controlflowapp/BackhroundThreadActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/BackhroundThreadActivity.kt
@@ -2,11 +2,23 @@ package id.co.iconpln.controlflowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_backhround_thread.*
 
-class BackhroundThreadActivity : AppCompatActivity() {
+class BackhroundThreadActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_backhround_thread)
+
+        btnThreadWorker.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btnThreadWorker -> {
+                
+            }
+        }
     }
 }
