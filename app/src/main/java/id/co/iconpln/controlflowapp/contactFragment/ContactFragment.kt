1cd429f9a68a2fc94f2f2049473e14b01d8af980
Fatima Az <azzahro.fatima@gmail.com>
2020-01-10 08:15:31 +0700

    [Izza] Contact Fragment - Show List Contact

--- app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactFragment.kt
@@ -6,13 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.contact.ContactAdapter
+import kotlinx.android.synthetic.main.fragment_contact.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class ContactFragment : Fragment() {
+
+    private lateinit var adapter: ContactAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +25,19 @@ class ContactFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showListContact()
+    }
+
+    private fun showListContact() {
+        adapter = ContactAdapter()
+        adapter.notifyDataSetChanged()
+
+        rvContactFragmentList.layoutManager = LinearLayoutManager(requireContext())
+        rvContactFragmentList.adapter = adapter
     }
     
 }
