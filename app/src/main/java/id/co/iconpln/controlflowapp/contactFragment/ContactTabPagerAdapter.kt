64004610d6f82491f6eac63c4955b610e44e315e
Fatima Az <azzahro.fatima@gmail.com>
2020-01-10 07:58:40 +0700

    [Izza] Tab Contact Adapter - Override Needed Function

--- app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabPagerAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabPagerAdapter.kt
@@ -1,9 +1,18 @@
 package id.co.iconpln.controlflowapp.contactFragment
 
 import android.content.Context
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
 class ContactTabPagerAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
