1397d78db5aa32ac2182673f675df8f910e1916b
Fatima Az <azzahro.fatima@gmail.com>
2020-01-10 08:18:19 +0700

    [Izza] Contact Fragment - Init View Model

--- app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 
 import id.co.iconpln.controlflowapp.R
@@ -19,6 +20,8 @@ class ContactFragment : Fragment() {
 
     private lateinit var adapter: ContactAdapter
 
+    private lateinit var contactFragmentViewModel: ContactFragmentViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,7 +32,14 @@ class ContactFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViewModel()
         showListContact()
+    }
+
+    private fun initViewModel() {
+        contactFragmentViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
+            .get(ContactFragmentViewModel::class.java)
     }
 
     private fun showListContact() {
