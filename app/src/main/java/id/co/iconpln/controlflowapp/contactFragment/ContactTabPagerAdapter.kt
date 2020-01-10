f1cfed34001cd535ad107ccb2269de6c41707414
Fatima Az <azzahro.fatima@gmail.com>
2020-01-10 07:57:19 +0700

    [Izza] Tab Contact Adapter - Add Initial Adapter

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabPagerAdapter.kt
@@ -0,0 +1,9 @@
+package id.co.iconpln.controlflowapp.contactFragment
+
+import android.content.Context
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ContactTabPagerAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+}
