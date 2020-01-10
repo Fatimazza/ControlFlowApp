434d524b0bef84170d82f7f26fa4acf0da350472
Fatima Az <azzahro.fatima@gmail.com>
2020-01-10 08:03:55 +0700

    [Izza] Tab Contact - Setup Tab Adapter

--- app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabActivity.kt
@@ -3,11 +3,22 @@ package id.co.iconpln.controlflowapp.contactFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_contact_tab.*
 
 class ContactTabActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_tab)
+
+        setupTab()
+    }
+
+    private fun setupTab() {
+        val tabPagerAdapter = ContactTabPagerAdapter(this, supportFragmentManager)
+        vpTabContact.adapter = tabPagerAdapter
+        tabContact.setupWithViewPager(vpTabContact)
+
+        supportActionBar?.elevation = 0f
     }
 }
