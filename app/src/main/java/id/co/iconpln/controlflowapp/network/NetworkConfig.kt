8104d72e102d10afeadfab8ed2d281cac0b46c9d
Fatima Az <azzahro.fatima@gmail.com>
2020-01-10 08:43:33 +0700

    [Izza] Network - Add Retrofit Config

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/network/NetworkConfig.kt
@@ -0,0 +1,31 @@
+package id.co.iconpln.controlflowapp.network
+
+import id.co.iconpln.controlflowapp.BuildConfig
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NetworkConfig {
+
+    companion object {
+
+        @Volatile
+        private var retrofit: Retrofit? = null
+
+        private fun getRetrofit(): Retrofit {
+            return retrofit ?: synchronized(this) {
+                retrofit ?: buildRetrofit().also {
+                    retrofit = it
+                }
+            }
+        }
+
+        private fun buildRetrofit(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BuildConfig.CONTACT_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+    }
+
+}
