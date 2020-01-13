d15be526370a325d23ada1f997cbbe84cb4a3318
Fatima Az <azzahro.fatima@gmail.com>
2020-01-13 07:20:13 +0700

    [Izza] My Contact - Show List Contact in Activity

--- app/src/main/java/id/co/iconpln/controlflowapp/myContact/MyContactActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myContact/MyContactActivity.kt
@@ -2,12 +2,26 @@ package id.co.iconpln.controlflowapp.myContact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_my_contact.*
 
 class MyContactActivity : AppCompatActivity() {
+
+    private lateinit var adapter: MyContactAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_contact)
+
+        showListContact()
+    }
+
+    private fun showListContact() {
+        adapter = MyContactAdapter()
+        adapter.notifyDataSetChanged()
+
+        rvMyContactList.layoutManager = LinearLayoutManager(this)
+        rvMyContactList.adapter = adapter
     }
 }
