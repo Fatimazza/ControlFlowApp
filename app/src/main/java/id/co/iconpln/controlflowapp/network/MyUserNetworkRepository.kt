89818c10ba6ea1d18f230cda1c2601c02795dcea
Fatima Az <azzahro.fatima@gmail.com>
2020-01-13 10:01:54 +0700

    [Izza] My User Repository - Get User Response from Network

--- app/src/main/java/id/co/iconpln/controlflowapp/network/MyUserNetworkRepository.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/network/MyUserNetworkRepository.kt
@@ -3,7 +3,9 @@ package id.co.iconpln.controlflowapp.network
 import androidx.lifecycle.MutableLiveData
 import id.co.iconpln.controlflowapp.model.myUser.BaseUserResponse
 import id.co.iconpln.controlflowapp.model.myUser.UserDataResponse
+import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.Response
 
 class MyUserNetworkRepository {
 
@@ -12,7 +14,24 @@ class MyUserNetworkRepository {
         val usersData = MutableLiveData<ArrayList<UserDataResponse>>()
         val listUser = ArrayList<UserDataResponse>()
 
-        NetworkConfig.userApi().getAllUsers().enqueue(object : Callback<BaseUserResponse<UserDataResponse>> {})
+        NetworkConfig.userApi().getAllUsers().enqueue(object : Callback<BaseUserResponse<UserDataResponse>> {
+            override fun onFailure(call: Call<BaseUserResponse<UserDataResponse>>, t: Throwable) {
+                usersData.postValue(null)
+            }
+
+            override fun onResponse(
+                call: Call<BaseUserResponse<UserDataResponse>>,
+                response: Response<BaseUserResponse<UserDataResponse>>
+            ) {
+                if (response.isSuccessful) {
+                    val listUserSize = response.body()?.data?.size as Int
+                    for (i in 0 until listUserSize) {
+                        listUser.add(response.body()?.data?.get(i) as UserDataResponse)
+                    }
+                    usersData.postValue(listUser)
+                }
+            }
+        })
 
         return usersData
     }
