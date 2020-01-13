9d53f4b8cbc798db4dfb923d4edcaf839cd0b5f9
Fatima Az <azzahro.fatima@gmail.com>
2020-01-13 10:04:13 +0700

    [Izza] My User View Model - Add function to get All Users from Network

--- app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserViewModel.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserViewModel.kt
@@ -1,7 +1,13 @@
 package id.co.iconpln.controlflowapp.myUser
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import id.co.iconpln.controlflowapp.model.myUser.UserDataResponse
+import id.co.iconpln.controlflowapp.network.MyUserNetworkRepository
 
 class MyUserViewModel : ViewModel() {
-    
+
+    fun getListUsers(): MutableLiveData<ArrayList<UserDataResponse>> {
+        return MyUserNetworkRepository().getAllUsers()
+    }
 }
