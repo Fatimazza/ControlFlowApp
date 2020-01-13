c49aebf91d10ad3dee45cbccb577fb2025ffac93
Fatima Az <azzahro.fatima@gmail.com>
2020-01-13 10:05:32 +0700

    [Izza] My User - Init View Model

--- app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserActivity.kt
@@ -3,6 +3,7 @@ package id.co.iconpln.controlflowapp.myUser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
 import kotlinx.android.synthetic.main.activity_my_user.*
@@ -11,12 +12,20 @@ private lateinit var adapter: MyUserAdapter
 
 class MyUserActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: MyUserViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_user)
 
+        initViewModel()
         showListUser()
         addListClickListener()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
+            .get(MyUserViewModel::class.java)
     }
 
     private fun showListUser() {
