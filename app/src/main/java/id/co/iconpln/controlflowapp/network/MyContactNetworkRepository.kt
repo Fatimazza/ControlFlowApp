23bfb824e7a81aaa980a89ea0471e865cb354b7e
Fatima Az <azzahro.fatima@gmail.com>
2020-01-13 07:50:31 +0700

    [Izza] My Contact Repository - Add function to fetch Contact from Network

--- app/src/main/java/id/co/iconpln/controlflowapp/network/MyContactNetworkRepository.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/network/MyContactNetworkRepository.kt
@@ -1,5 +1,18 @@
 package id.co.iconpln.controlflowapp.network
 
+import androidx.lifecycle.MutableLiveData
+import id.co.iconpln.controlflowapp.model.myContact.BaseContactResponse
+import id.co.iconpln.controlflowapp.model.myContact.ContactResponse
+import retrofit2.Callback
+
 class MyContactNetworkRepository {
 
+    fun getContacts(): MutableLiveData<ArrayList<ContactResponse>> {
+        
+        val contactsData = MutableLiveData<ArrayList<ContactResponse>>()
+
+        NetworkConfig.contactApi().fetchContacts().enqueue(object : Callback<BaseContactResponse<ContactResponse>> {})
+
+        return contactsData
+    }
 }
