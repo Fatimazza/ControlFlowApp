6e960782b9ef62a8c6ac15eb60a2f6af9056d406
Fatima Az <azzahro.fatima@gmail.com>
2020-01-13 07:23:11 +0700

    [Izza] My Contact - Init View Model

--- app/src/main/java/id/co/iconpln/controlflowapp/myContact/MyContactActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myContact/MyContactActivity.kt
@@ -2,6 +2,7 @@ package id.co.iconpln.controlflowapp.myContact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
 import kotlinx.android.synthetic.main.activity_my_contact.*
@@ -10,11 +11,19 @@ class MyContactActivity : AppCompatActivity() {
 
     private lateinit var adapter: MyContactAdapter
 
+    private lateinit var viewModel: MyContactViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_contact)
 
+        initViewModel()
         showListContact()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
+            .get(MyContactViewModel::class.java)
     }
 
     private fun showListContact() {
