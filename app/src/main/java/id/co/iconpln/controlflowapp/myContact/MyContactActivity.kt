55398ebdad1a8b10c51b6731a4b3625eb8bf4c3e
Fatima Az <azzahro.fatima@gmail.com>
2020-01-13 07:14:29 +0700

    [Izza] My Contact - Add Activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/myContact/MyContactActivity.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.myContact
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.iconpln.controlflowapp.R
+
+class MyContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_contact)
+    }
+}
