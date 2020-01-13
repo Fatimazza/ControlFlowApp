63475f4acaee795bff7f3d747afdae38a6b287da
Fatima Az <azzahro.fatima@gmail.com>
2020-01-13 09:27:05 +0700

    [Izza] My User - Show List User

--- app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserActivity.kt
@@ -2,12 +2,26 @@ package id.co.iconpln.controlflowapp.myUser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_my_user.*
+
+private lateinit var adapter: MyUserAdapter
 
 class MyUserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_user)
+
+        showListUser()
+    }
+
+    private fun showListUser() {
+        adapter = MyUserAdapter()
+        adapter.notifyDataSetChanged()
+
+        rvMyUserList.layoutManager = LinearLayoutManager(this)
+        rvMyUserList.adapter = adapter
     }
 }
