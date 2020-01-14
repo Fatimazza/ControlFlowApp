476b80e5c60b3c20e193b03268b161691d12c489
Fatima Az <azzahro.fatima@gmail.com>
2020-01-14 07:39:39 +0700

    [Izza] My User - Delete Unused Model

--- app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUser.kt
+++
@@ -1,8 +0,0 @@
-package id.co.iconpln.controlflowapp.myUser
-
-data class MyUser(
-    var id: String = "0",
-    var name: String? = null,
-    var address: String? = null,
-    var phone: String? = null
-)
