f59ff8344a30586296263a645f35510dc97e73ef
Fatima Az <azzahro.fatima@gmail.com>
2020-01-14 08:37:57 +0700

    [Izza] My User Form - Call View Model to update User

--- app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserFormActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserFormActivity.kt
@@ -3,6 +3,8 @@ package id.co.iconpln.controlflowapp.myUser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import id.co.iconpln.controlflowapp.R
 import id.co.iconpln.controlflowapp.model.myUser.UserDataResponse
@@ -17,6 +19,8 @@ class MyUserFormActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var user: UserDataResponse
 
     private lateinit var viewModel: MyUserFormViewModel
+
+    private var userId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,13 +49,27 @@ class MyUserFormActivity : AppCompatActivity(), View.OnClickListener {
         etUserFormName.setText(user.name)
         etUserFormAddress.setText(user.address)
         etUserFormHp.setText(user.phone)
+        userId = user.id
     }
 
     override fun onClick(view: View) {
         when (view.id) {
             R.id.btnUserFormSave -> {
-                
+                if (userId != null) {
+                    updateUser(userId as Int)
+                }
             }
         }
+    }
+
+    private fun updateUser(id: Int) {
+        //get result from View Model's Live Data
+        viewModel.updateUser(id).observe(this, Observer { userData ->
+            if (userData != null) {
+                Toast.makeText(this, "User Updated Successfully!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Failed to update User", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
