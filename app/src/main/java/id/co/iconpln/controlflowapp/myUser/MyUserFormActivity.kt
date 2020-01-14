c7812c868d47bfbbee8d2708f76e449516b0c993
Fatima Az <azzahro.fatima@gmail.com>
2020-01-14 07:57:56 +0700

    [Izza] My User Form - Populate User Data

--- app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserFormActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserFormActivity.kt
@@ -3,6 +3,8 @@ package id.co.iconpln.controlflowapp.myUser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.model.myUser.UserDataResponse
+import kotlinx.android.synthetic.main.activity_my_user_form.*
 
 class MyUserFormActivity : AppCompatActivity() {
 
@@ -10,8 +12,23 @@ class MyUserFormActivity : AppCompatActivity() {
         const val EXTRA_USER = "extra_user"
     }
 
+    private lateinit var user: UserDataResponse
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_user_form)
+
+        initIntentExtra()
+        populateFormData(user)
+    }
+
+    private fun initIntentExtra() {
+        user = intent.getParcelableExtra(EXTRA_USER)
+    }
+
+    private fun populateFormData(user: UserDataResponse) {
+        etUserFormName.setText(user.name)
+        etUserFormAddress.setText(user.address)
+        etUserFormHp.setText(user.phone)
     }
 }
