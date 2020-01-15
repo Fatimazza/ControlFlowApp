ae767abc453f45fada72a78236cec7f3862a3013
Fatima Az <azzahro.fatima@gmail.com>
2020-01-15 09:25:47 +0700

    [Izza] My User Model - Add Model for Get Single User

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/model/myUser/Data.kt
@@ -0,0 +1,8 @@
+package id.co.iconpln.controlflowapp.model.myUser
+
+data class Data(
+    val address: String,
+    val id: Int,
+    val name: String,
+    val phone: String
+)
---
+++ app/src/main/java/id/co/iconpln/controlflowapp/model/myUser/SingleUserResponse.kt
@@ -0,0 +1,5 @@
+package id.co.iconpln.controlflowapp.model.myUser
+
+data class SingleUserResponse(
+    val `data`: Data
+)
