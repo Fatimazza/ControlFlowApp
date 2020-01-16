b55bf6b59a0d6815b5eb817138b7244c49953b97
Fatima Az <azzahro.fatima@gmail.com>
2020-01-16 08:14:35 +0700

    [Izza] My User Fav - Add Database Repository

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/database/FavoriteDatabaseRepository.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.database
+
+import androidx.lifecycle.LiveData
+
+class FavoriteDatabaseRepository(private val favDatabaseDao: FavoriteDatabaseDao) {
+
+    val allFavUsers: LiveData<List<FavoriteUser>> = favDatabaseDao.getAllUsers()
+
+    fun insertUser(user: FavoriteUser) {
+        favDatabaseDao.insertUser(user)
+    }
+
+}
