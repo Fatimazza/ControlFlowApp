c4972a7a483c9cd7bd804ad0d625884ddc9f1242
Fatima Az <azzahro.fatima@gmail.com>
2020-01-16 18:25:59 +0700

    [Izza] My User Fav - Add List Item Click Listener to open Form

--- app/src/main/java/id/co/iconpln/controlflowapp/myUserFavorite/MyUserFavoriteActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUserFavorite/MyUserFavoriteActivity.kt
@@ -1,12 +1,15 @@
 package id.co.iconpln.controlflowapp.myUserFavorite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.database.FavoriteUser
 import id.co.iconpln.controlflowapp.database.FavoriteViewModel
+import id.co.iconpln.controlflowapp.myUserForm.MyUserFormActivity
 import kotlinx.android.synthetic.main.activity_my_user_favorite.*
 
 class MyUserFavoriteActivity : AppCompatActivity() {
@@ -22,6 +25,7 @@ class MyUserFavoriteActivity : AppCompatActivity() {
         initViewModel()
         setActionBarTitle()
         showListUser()
+        addListClickListener()
         fetchFavoriteMovieData()
     }
 
@@ -48,6 +52,21 @@ class MyUserFavoriteActivity : AppCompatActivity() {
                 adapter.setData(listFavUser)
             }
         })
+    }
+
+    private fun addListClickListener() {
+        adapter.setOnItemClickCallback(object : MyUserFavoriteAdapter.OnItemClickCallback {
+            override fun onItemClick(myUser: FavoriteUser) {
+                openUserForm(myUser)
+            }
+        })
+    }
+
+    private fun openUserForm(myUser: FavoriteUser) {
+        val userFormIntent = Intent(this, MyUserFormActivity::class.java)
+        userFormIntent.putExtra(MyUserFormActivity.EXTRA_USER_ID, myUser.userId.toInt())
+        userFormIntent.putExtra(MyUserFormActivity.EXTRA_USER_EDIT, true)
+        startActivity(userFormIntent)
     }
 
 }
