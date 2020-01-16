76394c05e97ee0d1638569f80136c6a8f77a8619
Fatima Az <azzahro.fatima@gmail.com>
2020-01-16 07:09:42 +0700

    [Izza] My User Fav - Show List User

--- app/src/main/java/id/co/iconpln/controlflowapp/myUserFavorite/MyUserFavoriteActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUserFavorite/MyUserFavoriteActivity.kt
@@ -2,12 +2,27 @@ package id.co.iconpln.controlflowapp.myUserFavorite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.myUser.MyUserAdapter
+import kotlinx.android.synthetic.main.activity_my_user_favorite.*
 
 class MyUserFavoriteActivity : AppCompatActivity() {
+
+    private lateinit var adapter: MyUserAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_user_favorite)
+
+        showListUser()
+    }
+
+    private fun showListUser() {
+        adapter = MyUserAdapter()
+        adapter.notifyDataSetChanged()
+
+        rvMyUserFavList.layoutManager = LinearLayoutManager(this)
+        rvMyUserFavList.adapter = adapter
     }
 }
