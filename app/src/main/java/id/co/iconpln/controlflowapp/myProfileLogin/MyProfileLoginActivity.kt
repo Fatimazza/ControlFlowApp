89387dafd9d2227e3f8429556c56b1e07d796a46
Fatima Az <azzahro.fatima@gmail.com>
2020-01-21 08:08:25 +0700

    My Profile Login - Add Title, Add Button Click Listener

--- app/src/main/java/id/co/iconpln/controlflowapp/myProfileLogin/MyProfileLoginActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myProfileLogin/MyProfileLoginActivity.kt
@@ -2,12 +2,25 @@ package id.co.iconpln.controlflowapp.myProfileLogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_my_profile_login.*
 
-class MyProfileLoginActivity : AppCompatActivity() {
+class MyProfileLoginActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile_login)
+
+        supportActionBar?.title = "My Profile Login"
+        btnProfileLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id) {
+            R.id.btnProfileLogin -> {
+
+            }
+        }
     }
 }
