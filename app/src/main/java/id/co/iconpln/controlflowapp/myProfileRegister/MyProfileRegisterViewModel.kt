f2789bc21adb1b9b403efd4caa6ff7afe888d665
Fatima Az <azzahro.fatima@gmail.com>
2020-01-21 13:59:33 +0700

    My Profile Register - Add function Register in View Model

--- app/src/main/java/id/co/iconpln/controlflowapp/myProfileRegister/MyProfileRegisterViewModel.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myProfileRegister/MyProfileRegisterViewModel.kt
@@ -1,7 +1,17 @@
 package id.co.iconpln.controlflowapp.myProfileRegister
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import id.co.iconpln.controlflowapp.model.myProfile.ProfileLoginUser
+import id.co.iconpln.controlflowapp.model.myProfile.ProfileRegisterUser
+import id.co.iconpln.controlflowapp.model.myProfile.ProfileResponse
+import id.co.iconpln.controlflowapp.network.MyProfileNetworkRepository
 
 class MyProfileRegisterViewModel : ViewModel() {
+
+    fun register(profileRegisterUser: ProfileRegisterUser)
+            : MutableLiveData<ProfileResponse> {
+        return MyProfileNetworkRepository().doRegister(profileRegisterUser)
+    }
 
 }
