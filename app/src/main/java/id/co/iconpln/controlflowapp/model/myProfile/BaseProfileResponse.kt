6c0b044babd3ec7638e5255a2ef119daff7bdf3a
Fatima Az <azzahro.fatima@gmail.com>
2020-01-21 08:18:11 +0700

    My Profile Login - Add Model

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/model/myProfile/BaseProfileResponse.kt
@@ -0,0 +1,8 @@
+package id.co.iconpln.controlflowapp.model.myProfile
+
+data class BaseProfileResponse(
+    val `data`: Data,
+    val messages: List<String>,
+    val status: Int,
+    val success: Boolean
+)
---
+++ app/src/main/java/id/co/iconpln/controlflowapp/model/myProfile/Data.kt
@@ -0,0 +1,6 @@
+package id.co.iconpln.controlflowapp.model.myProfile
+
+data class Data(
+    val customer: Customer,
+    val token: String
+)
