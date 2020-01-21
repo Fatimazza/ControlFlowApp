96289f0d89bd3cac82f6c7b4ae7c321e73565dc6
Fatima Az <azzahro.fatima@gmail.com>
2020-01-21 13:57:50 +0700

    My Profile Register - Add and Init View Model

--- app/src/main/java/id/co/iconpln/controlflowapp/myProfileRegister/MyProfileRegisterActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myProfileRegister/MyProfileRegisterActivity.kt
@@ -2,12 +2,22 @@ package id.co.iconpln.controlflowapp.myProfileRegister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import id.co.iconpln.controlflowapp.R
 
 class MyProfileRegisterActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: MyProfileRegisterViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile_register)
+
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
+            .get(MyProfileRegisterViewModel::class.java)
     }
 }
