d955b5af558d9edc77c59b9f71fb90c9d7a1f80f
Fatima Az <azzahro.fatima@gmail.com>
2020-01-22 09:21:10 +0700

    My Profile - Add function to get Profile in View Model

--- app/src/main/java/id/co/iconpln/controlflowapp/MyProfile/MyProfileViewModel.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/MyProfile/MyProfileViewModel.kt
@@ -1,7 +1,14 @@
 package id.co.iconpln.controlflowapp.MyProfile
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import id.co.iconpln.controlflowapp.model.myProfile.ProfileResponse
+import id.co.iconpln.controlflowapp.network.MyProfileNetworkRepository
 
 class MyProfileViewModel : ViewModel() {
 
+    fun getProfile(token: String): MutableLiveData<ProfileResponse> {
+        return MyProfileNetworkRepository().getProfile(token)
+    }
+    
 }
