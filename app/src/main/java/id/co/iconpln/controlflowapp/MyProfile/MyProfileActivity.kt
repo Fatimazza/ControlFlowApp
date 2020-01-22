444950e0682d40ad07f7533310863e0e68c70082
Fatima Az <azzahro.fatima@gmail.com>
2020-01-22 07:04:34 +0700

    My Profile - Init Intent Extra

--- app/src/main/java/id/co/iconpln/controlflowapp/MyProfile/MyProfileActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/MyProfile/MyProfileActivity.kt
@@ -3,6 +3,7 @@ package id.co.iconpln.controlflowapp.MyProfile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.model.myProfile.ProfileLoginResponse
 
 class MyProfileActivity : AppCompatActivity() {
 
@@ -10,8 +11,18 @@ class MyProfileActivity : AppCompatActivity() {
         const val EXTRA_PROFILE = "extra_profile"
     }
 
+    private var profile: ProfileLoginResponse? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
+
+        initIntentExtra()
+    }
+
+    private fun initIntentExtra() {
+        if (intent.hasExtra(EXTRA_PROFILE)) {
+            profile = intent.getParcelableExtra(EXTRA_PROFILE)
+        }
     }
 }
