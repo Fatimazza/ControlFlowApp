527c0f0105b2dbda4e3344c3bb1765ed18e6fb46
Fatima Az <azzahro.fatima@gmail.com>
2020-01-22 07:51:38 +0700

    My Profile - Open Login Page

--- app/src/main/java/id/co/iconpln/controlflowapp/MyProfile/MyProfileActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/MyProfile/MyProfileActivity.kt
@@ -1,11 +1,16 @@
 package id.co.iconpln.controlflowapp.MyProfile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import id.co.iconpln.controlflowapp.R
 import id.co.iconpln.controlflowapp.model.myProfile.ProfileLoginResponse
+import id.co.iconpln.controlflowapp.myProfileLogin.MyProfileLoginActivity
+import kotlinx.android.synthetic.main.activity_my_profile.*
+import kotlinx.android.synthetic.main.activity_my_profile_login.*
 
-class MyProfileActivity : AppCompatActivity() {
+class MyProfileActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object {
         const val EXTRA_PROFILE = "extra_profile"
@@ -19,6 +24,11 @@ class MyProfileActivity : AppCompatActivity() {
 
         setupActionBar()
         initIntentExtra()
+        setClickListener()
+    }
+
+    private fun setClickListener() {
+        btnProfileToLogin.setOnClickListener(this)
     }
 
     private fun setupActionBar() {
@@ -28,6 +38,15 @@ class MyProfileActivity : AppCompatActivity() {
     private fun initIntentExtra() {
         if (intent.hasExtra(EXTRA_PROFILE)) {
             profileLoginResponse = intent.getParcelableExtra(EXTRA_PROFILE)
+        }
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btnProfileToLogin -> {
+                val loginIntent = Intent(this, MyProfileLoginActivity::class.java)
+                startActivity(loginIntent)
+            }
         }
     }
 }
