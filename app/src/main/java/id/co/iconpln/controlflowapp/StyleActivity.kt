62829a9cb0e9e42d5c94d815e7da817428ecd09d
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-06 16:13:39 +0700

    Style - Implement View Binding

--- app/src/main/java/id/co/iconpln/controlflowapp/StyleActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/StyleActivity.kt
@@ -2,11 +2,16 @@ package id.co.iconpln.controlflowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.co.iconpln.controlflowapp.databinding.ActivityStyleBinding
 
 class StyleActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityStyleBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_style)
+
+        binding = ActivityStyleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
