3fdaf3dbe5d1873fb46587f96fc64c2ef9673e3c
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-06 17:02:45 +0700

    Intent Move Object - Improve logic receiving Intent extras and showing data

--- app/src/main/java/id/co/iconpln/controlflowapp/IntentMoveObjectActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/IntentMoveObjectActivity.kt
@@ -1,5 +1,6 @@
 package id.co.iconpln.controlflowapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.co.iconpln.controlflowapp.databinding.ActivityIntentMoveObjectBinding
@@ -13,25 +14,27 @@ class IntentMoveObjectActivity : AppCompatActivity() {
         const val EXTRA_PERSON = "extra_person"
     }
 
-    private lateinit var person: Person
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityIntentMoveObjectBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        getIntentExtras()
         showData()
     }
 
-    private fun getIntentExtras() {
-        person = intent.getParcelableExtra(EXTRA_PERSON) ?: Person()
-    }
-
     private fun showData() {
-        val text =
-            "Name ${person.name}, \nAge ${person.age}, \nEmail ${person.email} \nCity ${person.city}"
-        binding.tvObjectReceived.text = text
+        val person = if (Build.VERSION.SDK_INT >= 33) {
+            intent.getParcelableExtra<Person>(EXTRA_PERSON, Person::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getParcelableExtra<Person>(EXTRA_PERSON)
+        }
+
+        if (person != null) {
+            val text =
+                "Name : ${person.name},\nEmail : ${person.email},\nAge : ${person.age},\nLocation : ${person.city}"
+            binding.tvObjectReceived.text = text
+        }
     }
 }
--- app/src/main/java/id/co/iconpln/controlflowapp/model/Person.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/model/Person.kt
@@ -4,9 +4,9 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Person (
+data class Person(
     var name: String = "",
     var age: Int = 0,
     var email: String = "",
     var city: String = ""
-): Parcelable
+) : Parcelable
