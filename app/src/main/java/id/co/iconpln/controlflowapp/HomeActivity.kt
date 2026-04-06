a25258e14a908b780c99e5598b30bbd3a2be1a90
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-06 15:16:51 +0700

    Home - Implement View Binding and Remove deprecated Kotlin synthetic

--- app/src/main/java/id/co/iconpln/controlflowapp/HomeActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/HomeActivity.kt
@@ -8,6 +8,7 @@ import id.co.iconpln.controlflowapp.MyProfile.MyProfileActivity
 import id.co.iconpln.controlflowapp.bottomSheetDialog.BottomSheetActivity
 import id.co.iconpln.controlflowapp.contact.ContactActivity
 import id.co.iconpln.controlflowapp.contactFragment.ContactTabActivity
+import id.co.iconpln.controlflowapp.databinding.ActivityHomeBinding
 import id.co.iconpln.controlflowapp.fragmentBottomNav.BottomNavActivity
 import id.co.iconpln.controlflowapp.fragmentNavDrawer.NavDrawerActivity
 import id.co.iconpln.controlflowapp.fragments.DemoFragmentActivity
@@ -15,153 +16,180 @@ import id.co.iconpln.controlflowapp.hero.ListHeroActivity
 import id.co.iconpln.controlflowapp.fragmentTab.TabActivity
 import id.co.iconpln.controlflowapp.fragmentViewPager.ScrollActivity
 import id.co.iconpln.controlflowapp.myContact.MyContactActivity
-import id.co.iconpln.controlflowapp.myProfileLogin.MyProfileLoginActivity
 import id.co.iconpln.controlflowapp.myUser.MyUserActivity
 import id.co.iconpln.controlflowapp.sharedPreference.SharedPreferenceActivity
 import id.co.iconpln.controlflowapp.weather.WeatherActivity
-import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setClickListener()
     }
 
     private fun setClickListener() {
-        btnCalculation.setOnClickListener(this)
-        btnClassification.setOnClickListener(this)
-        btnLogin.setOnClickListener(this)
-        btnOperation.setOnClickListener(this)
-        btnStyle.setOnClickListener(this)
-        btnActivity.setOnClickListener(this)
-        btnVolumeActivity.setOnClickListener(this)
-        btnIntent.setOnClickListener(this)
-        btnConstraint.setOnClickListener(this)
-        btnComplexConstraint.setOnClickListener(this)
-        btnRecyclerView.setOnClickListener(this)
-        btnFragment.setOnClickListener(this)
-        btnTab.setOnClickListener(this)
-        btnBottomNav.setOnClickListener(this)
-        btnNavDrawer.setOnClickListener(this)
-        btnBottomSheetDemo.setOnClickListener(this)
-        btnViewPager.setOnClickListener(this)
-        btnLocalization.setOnClickListener(this)
-        btnSharedPreference.setOnClickListener(this)
-        btnWeather.setOnClickListener(this)
-        btnContact.setOnClickListener(this)
-        btnThread.setOnClickListener(this)
-        btnContactFragment.setOnClickListener(this)
-        btnMyContact.setOnClickListener(this)
-        btnMyUser.setOnClickListener(this)
-        btnMyProfile.setOnClickListener(this)
+        binding.btnCalculation.setOnClickListener(this)
+        binding.btnClassification.setOnClickListener(this)
+        binding.btnLogin.setOnClickListener(this)
+        binding.btnOperation.setOnClickListener(this)
+        binding.btnStyle.setOnClickListener(this)
+        binding.btnActivity.setOnClickListener(this)
+        binding.btnVolumeActivity.setOnClickListener(this)
+        binding.btnIntent.setOnClickListener(this)
+        binding.btnConstraint.setOnClickListener(this)
+        binding.btnComplexConstraint.setOnClickListener(this)
+        binding.btnRecyclerView.setOnClickListener(this)
+        binding.btnFragment.setOnClickListener(this)
+        binding.btnTab.setOnClickListener(this)
+        binding.btnBottomNav.setOnClickListener(this)
+        binding.btnNavDrawer.setOnClickListener(this)
+        binding.btnBottomSheetDemo.setOnClickListener(this)
+        binding.btnViewPager.setOnClickListener(this)
+        binding.btnLocalization.setOnClickListener(this)
+        binding.btnSharedPreference.setOnClickListener(this)
+        binding.btnWeather.setOnClickListener(this)
+        binding.btnContact.setOnClickListener(this)
+        binding.btnThread.setOnClickListener(this)
+        binding.btnContactFragment.setOnClickListener(this)
+        binding.btnMyContact.setOnClickListener(this)
+        binding.btnMyUser.setOnClickListener(this)
+        binding.btnMyProfile.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
-        when (view.id) {
-            R.id.btnCalculation -> {
+        when (view) {
+            binding.btnCalculation -> {
                 val calculationIntent = Intent(this, MainActivity::class.java)
                 startActivity(calculationIntent)
             }
-            R.id.btnClassification -> {
+
+            binding.btnClassification -> {
                 val classificationIntent = Intent(this, ClassificationActivity::class.java)
                 startActivity(classificationIntent)
             }
-            R.id.btnLogin -> {
+
+            binding.btnLogin -> {
                 val loginIntent = Intent(this, LoginActivity::class.java)
                 startActivity(loginIntent)
             }
-            R.id.btnOperation -> {
+
+            binding.btnOperation -> {
                 val operationIntent = Intent(this, OperationActivity::class.java)
                 startActivity(operationIntent)
             }
-            R.id.btnStyle -> {
+
+            binding.btnStyle -> {
                 val styleIntent = Intent(this, StyleActivity::class.java)
                 startActivity(styleIntent)
             }
-            R.id.btnActivity -> {
+
+            binding.btnActivity -> {
                 val demoIntent = Intent(this, DemoActivity::class.java)
                 startActivity(demoIntent)
             }
-            R.id.btnVolumeActivity -> {
+
+            binding.btnVolumeActivity -> {
                 val volumeIntent = Intent(this, VolumeActivity::class.java)
                 startActivity(volumeIntent)
             }
-            R.id.btnIntent -> {
+
+            binding.btnIntent -> {
                 val intent = Intent(this, IntentActivity::class.java)
                 startActivity(intent)
             }
-            R.id.btnConstraint -> {
+
+            binding.btnConstraint -> {
                 val constraintIntent = Intent(this, ConstraintActivity::class.java)
                 startActivity(constraintIntent)
             }
-            R.id.btnComplexConstraint -> {
+
+            binding.btnComplexConstraint -> {
                 val complexConstraintIntent = Intent(this, ComplexConstraintActivity::class.java)
                 startActivity(complexConstraintIntent)
             }
-            R.id.btnRecyclerView -> {
+
+            binding.btnRecyclerView -> {
                 val listHeroIntent = Intent(this, ListHeroActivity::class.java)
                 startActivity(listHeroIntent)
             }
-            R.id.btnFragment -> {
+
+            binding.btnFragment -> {
                 val demoFragment = Intent(this, DemoFragmentActivity::class.java)
                 startActivity(demoFragment)
             }
-            R.id.btnTab -> {
+
+            binding.btnTab -> {
                 val tabIntent = Intent(this, TabActivity::class.java)
                 startActivity(tabIntent)
             }
-            R.id.btnBottomNav -> {
+
+            binding.btnBottomNav -> {
                 val bottomNavIntent = Intent(this, BottomNavActivity::class.java)
                 startActivity(bottomNavIntent)
             }
-            R.id.btnNavDrawer -> {
+
+            binding.btnNavDrawer -> {
                 val navDrawerIntent = Intent(this, NavDrawerActivity::class.java)
                 startActivity(navDrawerIntent)
             }
-            R.id.btnBottomSheetDemo -> {
+
+            binding.btnBottomSheetDemo -> {
                 val bottomSheetIntent = Intent(this, BottomSheetActivity::class.java)
                 startActivity(bottomSheetIntent)
             }
-            R.id.btnViewPager -> {
+
+            binding.btnViewPager -> {
                 val viewPagerIntent = Intent(this, ScrollActivity::class.java)
                 startActivity(viewPagerIntent)
             }
-            R.id.btnLocalization -> {
+
+            binding.btnLocalization -> {
                 val localizationIntent = Intent(this, LocalizationActivity::class.java)
                 startActivity(localizationIntent)
             }
-            R.id.btnSharedPreference -> {
+
+            binding.btnSharedPreference -> {
                 val sharedPrefIntent = Intent(this, SharedPreferenceActivity::class.java)
                 startActivity(sharedPrefIntent)
             }
-            R.id.btnWeather -> {
+
+            binding.btnWeather -> {
                 val weatherIntent = Intent(this, WeatherActivity::class.java)
                 startActivity(weatherIntent)
             }
-            R.id.btnContact -> {
+
+            binding.btnContact -> {
                 val contactIntent = Intent(this, ContactActivity::class.java)
                 startActivity(contactIntent)
             }
-            R.id.btnThread -> {
-                val backgrounThreadIntent = Intent(this, BackgroundThreadActivity::class.java)
-                startActivity(backgrounThreadIntent)
+
+            binding.btnThread -> {
+                val backgroundThreadIntent = Intent(this, BackgroundThreadActivity::class.java)
+                startActivity(backgroundThreadIntent)
             }
-            R.id.btnContactFragment -> {
+
+            binding.btnContactFragment -> {
                 val contactFragmentIntent = Intent(this, ContactTabActivity::class.java)
                 startActivity(contactFragmentIntent)
             }
-            R.id.btnMyContact -> {
+
+            binding.btnMyContact -> {
                 val myContactIntent = Intent(this, MyContactActivity::class.java)
                 startActivity(myContactIntent)
             }
-            R.id.btnMyUser -> {
+
+            binding.btnMyUser -> {
                 val myUserIntent = Intent(this, MyUserActivity::class.java)
                 startActivity(myUserIntent)
             }
-            R.id.btnMyProfile -> {
+
+            binding.btnMyProfile -> {
                 val myProfileIntent = Intent(this, MyProfileActivity::class.java)
                 startActivity(myProfileIntent)
             }
