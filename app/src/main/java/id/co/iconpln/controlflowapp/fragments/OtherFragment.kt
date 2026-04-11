d2d5b8d0bc16e8a8e9b49f6c97fe5273aa1c19ea
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-11 09:01:23 +0700

    Other Fragment - Implement View Binding in Fragment

--- app/src/main/java/id/co/iconpln/controlflowapp/fragments/OtherFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragments/OtherFragment.kt
@@ -1,32 +1,33 @@
 package id.co.iconpln.controlflowapp.fragments
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.databinding.FragmentOtherBinding
 import id.co.iconpln.controlflowapp.fragments.LastFragment.Companion.message
-import kotlinx.android.synthetic.main.fragment_other.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class OtherFragment : Fragment(), View.OnClickListener {
 
+    private var _binding: FragmentOtherBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_other, container, false)
+        _binding = FragmentOtherBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btnLastFragment.setOnClickListener(this)
+        binding.btnLastFragment.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
@@ -51,5 +52,11 @@ class OtherFragment : Fragment(), View.OnClickListener {
                 fragmentTransaction?.commit()
             }
         }
+    }
+
+    // Clean up binding to prevent memory leaks
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
