7919e27152d8b2a6c964814e2509aa7a9f210041
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-11 08:57:08 +0700

    Home Fragment - Implement View Binding in Fragment

--- app/src/main/java/id/co/iconpln/controlflowapp/fragments/HomeFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragments/HomeFragment.kt
@@ -1,36 +1,37 @@
 package id.co.iconpln.controlflowapp.fragments
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import id.co.iconpln.controlflowapp.R
-import kotlinx.android.synthetic.main.fragment_home.*
+import id.co.iconpln.controlflowapp.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment(), View.OnClickListener {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btnOtherFragment.setOnClickListener(this)
+        binding.btnOtherFragment.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
-        when(view.id) {
-            R.id.btnOtherFragment -> {
+        when (view) {
+            binding.btnOtherFragment -> {
                 val fragmentManager = fragmentManager
                 val fragmentTransaction = fragmentManager?.beginTransaction()
 
@@ -42,4 +43,9 @@ class HomeFragment : Fragment(), View.OnClickListener {
         }
     }
     
+    // Clean up binding to prevent memory leaks
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
