83ab453cd6490cfbeb1570e358a91fb5886373fc
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-13 15:55:31 +0700

    Nav Component - Add Navigation from Other Nav Fragment to Last Nav Fragment

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentsNavComponent/OtherNavFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentsNavComponent/OtherNavFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import id.co.iconpln.controlflowapp.R
 import id.co.iconpln.controlflowapp.databinding.FragmentOtherNavBinding
 
 class OtherNavFragment : Fragment() {
@@ -20,6 +22,15 @@ class OtherNavFragment : Fragment() {
         _binding = FragmentOtherNavBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnLastFragment.setOnClickListener {
+            view -> view.findNavController().navigate(
+                R.id.action_otherNavFragment_to_lastNavFragment
+            )
+        }
     }
 
     // Clean up binding to prevent memory leaks
