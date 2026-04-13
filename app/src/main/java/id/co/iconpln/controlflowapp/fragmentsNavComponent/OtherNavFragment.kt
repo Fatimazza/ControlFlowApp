f8e6c9a5c23181b581a86c2aa0de0cd107c25537
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-13 14:41:15 +0700

    Nav Component - Add Other Nav Fragment

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentsNavComponent/OtherNavFragment.kt
@@ -0,0 +1,30 @@
+package id.co.iconpln.controlflowapp.fragmentsNavComponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.co.iconpln.controlflowapp.databinding.FragmentOtherNavBinding
+
+class OtherNavFragment : Fragment() {
+
+    private var _binding: FragmentOtherNavBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentOtherNavBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    // Clean up binding to prevent memory leaks
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
