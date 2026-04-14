6ed252aae6e45e94870334c523a2a656462d8731
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-14 11:24:03 +0700

    Tab Adapter - Setup Fragments inside Adapter

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/SectionsPagerAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/SectionsPagerAdapter.kt
@@ -3,13 +3,19 @@ package id.co.iconpln.controlflowapp.fragmentTab
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import id.co.iconpln.controlflowapp.hero.ListHeroFragment
 
 class SectionsPagerAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity) {
     override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = ListHeroFragment()
+            1 -> fragment = SecondFragment()
+        }
+        return fragment as Fragment
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return 2
     }
 }
