fb5c293b47b5a5860a6c4ddb70f774cf6ae06d1e
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-14 11:03:17 +0700

    Tab Activity - Implement View Binding

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabActivity.kt
@@ -2,22 +2,25 @@ package id.co.iconpln.controlflowapp.fragmentTab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import id.co.iconpln.controlflowapp.R
-import kotlinx.android.synthetic.main.activity_tab.*
+import id.co.iconpln.controlflowapp.databinding.ActivityTabBinding
 
 class TabActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityTabBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tab)
+
+        binding = ActivityTabBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setupTab()
     }
 
     private fun setupTab() {
         val tabPagerAdapter = TabPagerAdapter(this, supportFragmentManager)
-        vpTab.adapter = tabPagerAdapter
-        tab.setupWithViewPager(vpTab)
+        binding.vpTab.adapter = tabPagerAdapter
+        binding.tab.setupWithViewPager(binding.vpTab)
 
         supportActionBar?.elevation = 0f
     }
