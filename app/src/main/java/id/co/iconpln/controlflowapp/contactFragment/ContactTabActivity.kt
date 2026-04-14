075cb9f0feb885db5babe1a8f6b61a3acfa4acae
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-14 12:43:50 +0700

    Tab Contact - Implement new Adapter and ViewPager2

--- app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabActivity.kt
@@ -2,9 +2,20 @@ package id.co.iconpln.controlflowapp.contactFragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.StringRes
+import com.google.android.material.tabs.TabLayoutMediator
+import id.co.iconpln.controlflowapp.R
 import id.co.iconpln.controlflowapp.databinding.ActivityContactTabBinding
 
 class ContactTabActivity : AppCompatActivity() {
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_contact,
+            R.string.tab_text_2
+        )
+    }
 
     private lateinit var binding: ActivityContactTabBinding
 
@@ -18,9 +29,15 @@ class ContactTabActivity : AppCompatActivity() {
     }
 
     private fun setupTab() {
-        val tabPagerAdapter = ContactTabPagerAdapter(this, supportFragmentManager)
-        binding.vpTabContact.adapter = tabPagerAdapter
-        binding.tabContact.setupWithViewPager(binding.vpTabContact)
+        val sectionsPagerAdapter = ContactSectionsPagerAdapter(this)
+
+        binding.vpTabContact.adapter = sectionsPagerAdapter
+        TabLayoutMediator(
+            binding.tabContact, binding.vpTabContact
+        )
+        { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
 
         supportActionBar?.elevation = 0f
     }
