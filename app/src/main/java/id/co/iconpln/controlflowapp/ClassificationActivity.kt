e7d0ad0567a33953d6d8047dde5dbc3c9981edc8
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-14 20:44:29 +0700

    Classification - Implement View Binding

--- app/src/main/java/id/co/iconpln/controlflowapp/ClassificationActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/ClassificationActivity.kt
@@ -3,38 +3,52 @@ package id.co.iconpln.controlflowapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_classification.*
+import id.co.iconpln.controlflowapp.databinding.ActivityClassificationBinding
 
 class ClassificationActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityClassificationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_classification)
+        binding = ActivityClassificationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        etClassificationNilai.setText("0")
-        btnClassificationShow.setOnClickListener {
+        binding.etClassificationNilai.setText("0")
+        binding.btnClassificationShow.setOnClickListener {
             checkField()
         }
     }
 
     fun checkField() {
-        if (etClassificationNilai.text.isNullOrEmpty()) {
+        if (binding.etClassificationNilai.text.isNullOrEmpty()) {
             Toast.makeText(this, "Nilai tidak boleh kosong", Toast.LENGTH_SHORT).show()
-        } else if (etClassificationNilai.text.toString().toInt() > 1000) {
+        } else if (binding.etClassificationNilai.text.toString().toInt() > 1000) {
             Toast.makeText(this, "Nilai tidak boleh lebih dari 1000", Toast.LENGTH_SHORT).show()
         } else {
-            doClassification(etClassificationNilai.text.toString().toInt())
+            doClassification(binding.etClassificationNilai.text.toString().toInt())
         }
     }
 
     fun doClassification(nilai: Int) {
         var hasil = ""
-        when(nilai) {
-            in 0..70 -> { hasil = "Hasilnya: Anda Belum Lulus" }
-            in 71..80 -> { hasil = "Hasilnya: Anda Lulus Aja" }
-            in 81..100 -> { hasil = "Hasilnya: Anda Lulus Banget" }
-            !in 0..100 -> { hasil = "Hasilnya: Nilai Error" }
+        when (nilai) {
+            in 0..70 -> {
+                hasil = "Hasilnya: Anda Belum Lulus"
+            }
+
+            in 71..80 -> {
+                hasil = "Hasilnya: Anda Lulus Aja"
+            }
+
+            in 81..100 -> {
+                hasil = "Hasilnya: Anda Lulus Banget"
+            }
+
+            !in 0..100 -> {
+                hasil = "Hasilnya: Nilai Error"
+            }
         }
-        tvClassificationHasil.text = hasil
+        binding.tvClassificationHasil.text = hasil
     }
 }
