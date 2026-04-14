291d691e3153e599f0282dfe0bce9c27a47f1eed
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-14 12:23:49 +0700

    Tab Contact - Implement View Binding

--- app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabActivity.kt
@@ -2,22 +2,25 @@ package id.co.iconpln.controlflowapp.contactFragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import id.co.iconpln.controlflowapp.R
-import kotlinx.android.synthetic.main.activity_contact_tab.*
+import id.co.iconpln.controlflowapp.databinding.ActivityContactTabBinding
 
 class ContactTabActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityContactTabBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact_tab)
+
+        binding = ActivityContactTabBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setupTab()
     }
 
     private fun setupTab() {
         val tabPagerAdapter = ContactTabPagerAdapter(this, supportFragmentManager)
-        vpTabContact.adapter = tabPagerAdapter
-        tabContact.setupWithViewPager(vpTabContact)
+        binding.vpTabContact.adapter = tabPagerAdapter
+        binding.tabContact.setupWithViewPager(binding.vpTabContact)
 
         supportActionBar?.elevation = 0f
     }
