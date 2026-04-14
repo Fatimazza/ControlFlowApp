6f7119134d43e6a80cc8265306162d8886a3ff7b
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-15 06:20:00 +0700

    Scroll View Pager - Implement View Binding in Fragment

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentViewPager/ScrollFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentViewPager/ScrollFragment.kt
@@ -1,26 +1,28 @@
 package id.co.iconpln.controlflowapp.fragmentViewPager
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import id.co.iconpln.controlflowapp.databinding.FragmentScrollBinding
 
-import id.co.iconpln.controlflowapp.R
-
-/**
- * A simple [Fragment] subclass.
- */
 class ScrollFragment : Fragment() {
+
+    private var _binding: FragmentScrollBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scroll, container, false)
+        _binding = FragmentScrollBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
