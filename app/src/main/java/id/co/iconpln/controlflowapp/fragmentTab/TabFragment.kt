90814294d67d4400bc5e7cdef1b7e1e567940b2f
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-14 12:15:41 +0700

    Tab Fragment - Implement new Adapter and ViewPager2

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabFragment.kt
@@ -5,12 +5,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import id.co.iconpln.controlflowapp.R
 import id.co.iconpln.controlflowapp.databinding.FragmentTabBinding
 
 
 class TabFragment : Fragment() {
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.menu_list_hero,
+            R.string.tab_text_2
+        )
+    }
 
     private var _binding: FragmentTabBinding? = null
     private val binding get() = _binding!!
@@ -28,17 +39,23 @@ class TabFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupTab()
+
+        (activity as AppCompatActivity).supportActionBar?.elevation = 0f
     }
 
     private fun setupTab() {
-        val tabPagerAdapter = TabPagerAdapter(requireContext(), childFragmentManager)
-        binding.vpTabFragment.adapter = tabPagerAdapter
-        binding.tabFragment.setupWithViewPager(binding.vpTabFragment)
+        val sectionsPagerAdapter = SectionsPagerAdapter(requireActivity() as AppCompatActivity)
+        binding.vpTabFragment.adapter = sectionsPagerAdapter
+        TabLayoutMediator(
+            binding.tabFragment, binding.vpTabFragment
+        )
+        { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        (activity as AppCompatActivity).supportActionBar?.elevation = 0f
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
