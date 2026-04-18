ab86a782af3bf80ed41c072120af9e2264208533
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 06:48:56 +0700

    Restaurant - Make Adapter Extends View Holder

--- app/src/main/java/id/co/iconpln/controlflowapp/restaurant/ui/RestaurantReviewAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/restaurant/ui/RestaurantReviewAdapter.kt
@@ -1,9 +1,26 @@
 package id.co.iconpln.controlflowapp.restaurant.ui
 
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import id.co.iconpln.controlflowapp.databinding.ItemReviewBinding
+import id.co.iconpln.controlflowapp.restaurant.data.response.CustomerReviewsItem
 
-class RestaurantReviewAdapter {
+class RestaurantReviewAdapter: ListAdapter<CustomerReviewsItem, RestaurantReviewAdapter.ReviewViewHolder> {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ReviewViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(
+        holder: ReviewViewHolder,
+        position: Int
+    ) {
+        TODO("Not yet implemented")
+    }
 
     class ReviewViewHolder(val binding: ItemReviewBinding): RecyclerView.ViewHolder(binding.root) {
 
