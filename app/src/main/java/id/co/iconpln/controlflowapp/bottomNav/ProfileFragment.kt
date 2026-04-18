89c58676a90119e5db6e9f4906a24aedfd51f65f
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 04:14:36 +0700

    New Bottom Nav - Add Profile Fragment

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/bottomNav/ProfileFragment.kt
@@ -0,0 +1,29 @@
+package id.co.iconpln.controlflowapp.bottomNav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.co.iconpln.controlflowapp.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
