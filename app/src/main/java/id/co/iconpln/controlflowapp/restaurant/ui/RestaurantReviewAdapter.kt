52b4dcabf6d4ce1f1da1f1fc73226b38ab2becfd
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 06:44:39 +0700

    Restaurant - Add Initial Restaurant Review Adapter

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/restaurant/ui/RestaurantReviewAdapter.kt
@@ -0,0 +1,11 @@
+package id.co.iconpln.controlflowapp.restaurant.ui
+
+import androidx.recyclerview.widget.RecyclerView
+import id.co.iconpln.controlflowapp.databinding.ItemReviewBinding
+
+class RestaurantReviewAdapter {
+
+    class ReviewViewHolder(val binding: ItemReviewBinding): RecyclerView.ViewHolder(binding.root) {
+
+    }
+}
