d684b4688945eab6398d8ce55226a8b0ba51f826
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-18 18:37:45 +0700

    New Bottom Nav - Add generated Bottom Navigation Activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/bottomNav/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.bottomNav.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/id/co/iconpln/controlflowapp/bottomNav/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.bottomNav.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
