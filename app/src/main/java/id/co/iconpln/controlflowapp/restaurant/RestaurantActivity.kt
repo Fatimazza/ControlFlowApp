7253a658c7e92ff9f124e07cf63689afbe5eeb80
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 07:11:11 +0700

    Restaurant - Setup Review Recycler View in Activity

--- app/src/main/java/id/co/iconpln/controlflowapp/restaurant/RestaurantActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/restaurant/RestaurantActivity.kt
@@ -1,7 +1,10 @@
 package id.co.iconpln.controlflowapp.restaurant
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.databinding.ActivityRestaurantBinding
 
 class RestaurantActivity : AppCompatActivity() {
@@ -13,5 +16,15 @@ class RestaurantActivity : AppCompatActivity() {
 
         binding = ActivityRestaurantBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
+        setupReviewRecyclerView()
+    }
+
+    private fun setupReviewRecyclerView() {
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvReview.layoutManager = layoutManager
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.rvReview.addItemDecoration(itemDecoration)
     }
 }
