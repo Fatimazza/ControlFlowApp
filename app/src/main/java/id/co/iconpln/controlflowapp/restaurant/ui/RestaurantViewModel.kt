c36b48f5a4f3aeffcdb13715108196eb14e3179e
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 12:27:51 +0700

    Restaurant Live Data - Add View Model

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/restaurant/ui/RestaurantViewModel.kt
@@ -0,0 +1,11 @@
+package id.co.iconpln.controlflowapp.restaurant.ui
+
+import androidx.lifecycle.ViewModel
+
+class RestaurantViewModel: ViewModel() {
+
+    companion object{
+        private const val TAG = "RestaurantViewModel"
+        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
+    }
+}
