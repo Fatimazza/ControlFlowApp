e843d0c187cec23f680a410986e532a6cd32da02
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 12:30:56 +0700

    Restaurant Live Data - Add needed variables in View Model

--- app/src/main/java/id/co/iconpln/controlflowapp/restaurant/ui/RestaurantViewModel.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/restaurant/ui/RestaurantViewModel.kt
@@ -1,6 +1,10 @@
 package id.co.iconpln.controlflowapp.restaurant.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import id.co.iconpln.controlflowapp.restaurant.data.response.CustomerReviewsItem
+import id.co.iconpln.controlflowapp.restaurant.data.response.Restaurant
 
 class RestaurantViewModel: ViewModel() {
 
@@ -8,4 +12,13 @@ class RestaurantViewModel: ViewModel() {
         private const val TAG = "RestaurantViewModel"
         private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
     }
+
+    private val _restaurant = MutableLiveData<Restaurant>()
+    val restaurant: LiveData<Restaurant> = _restaurant
+
+    private val _listReview = MutableLiveData<List<CustomerReviewsItem>>()
+    val listReview: LiveData<List<CustomerReviewsItem>> = _listReview
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
 }
