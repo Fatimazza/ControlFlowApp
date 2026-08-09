3ed0dd4ac5524a64dca1ef03510c64b8f92ad5e6
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-08-09 11:38:56 +0700

    List Fruit - Add a new activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/listFruit/ListFruitActivity.kt
@@ -0,0 +1,12 @@
+package id.co.iconpln.controlflowapp.listFruit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.co.iconpln.controlflowapp.R
+
+class ListFruitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_fruit)
+    }
+}
