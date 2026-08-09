24007fe88e28f648dbdb1792e6bf5a0beea1d109
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-08-09 12:24:47 +0700

    List Fruit - Add a List Fruit Adapter

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/listFruit/ListFruitAdapter.kt
@@ -0,0 +1,10 @@
+package id.co.iconpln.controlflowapp.listFruit
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ListFruitAdapter: RecyclerView.Adapter<ListFruitAdapter.ListFruitViewHolder> {
+
+    class ListFruitViewHolder {
+
+    }
+}
