ed5aca0e30d2dae93a554aa25c8cd08d1ed2ad73
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-08-09 13:00:02 +0700

    List Fruit - Implements all functions in Adapter

--- app/src/main/java/id/co/iconpln/controlflowapp/listFruit/ListFruitAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/listFruit/ListFruitAdapter.kt
@@ -1,30 +1,37 @@
 package id.co.iconpln.controlflowapp.listFruit
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.model.Fruit
 
-class ListFruitAdapter: RecyclerView.Adapter<ListFruitAdapter.ListFruitViewHolder>() {
+class ListFruitAdapter(
+    private val itemList: List<Fruit>
+) : RecyclerView.Adapter<ListFruitAdapter.ListFruitViewHolder>() {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ListFruitViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list_fruit, parent, false)
+        return ListFruitViewHolder(view)
     }
 
     override fun onBindViewHolder(
         holder: ListFruitViewHolder,
         position: Int
     ) {
-        TODO("Not yet implemented")
+        val item = itemList[position]
+        holder.nameText.text = item.name
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = itemList.size
 
     class ListFruitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val nameText: TextView = itemView.findViewById(R.id.tvFruitName)
     }
 }
